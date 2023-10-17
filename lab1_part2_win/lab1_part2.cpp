65a9295617e181a12fc0c4cc1ab950ea4d388a93
Darya2302 <119890590+Darya2302@users.noreply.github.com>
2023-10-17 23:19:22 +0300

    Add files via upload

---
+++ lab1_part2_win/lab1_part2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include<processthreadsapi.h>
+#include <Windows.h>
+
+// Флаг для завершения работы потоков
+bool exitFlag = false;
+
+// Функция потока, которая пишет случайное число через определенный интервал
+void* threadFunc(void* arg) {
+    double interval = *(double*)arg;
+
+    while (!exitFlag) {
+        double randomNum = (double)rand() / RAND_MAX;
+        std::cout << randomNum << std::endl;
+        sleep(1300);
+    }
+
+    pthread_exit(NULL);
+}
+
+// Обработчик сигнала SIGINT
+void sigintHandler(int signum) {
+    std::cout << "Received SIGINT signal. Exiting..." << std::endl;
+    exitFlag = true;
+}
+
+void reversString() {
+    while (TRUE) {
+
+        char str[250];
+        scanf_s("%s", str, 250);
+        printf("%s\n", _strrev(str));
+    }
+}
+
+int main() {
+
+    // Создаем поток
+    pthread_t thread;
+    pthread_create(&thread, NULL, threadFunc, NULL, 0, NULL);
+    pthread_create(&thread, NULL, reversString, NULL, 0, NULL);
+
+    // Устанавливаем обработчик сигнала SIGINT
+    signal(SIGINT, sigintHandler);
+
+    // Ожидаем завершение работы потока
+    pthread_join(thread, NULL);
+
+    return 0;
+}
