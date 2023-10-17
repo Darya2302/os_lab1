658183287ff2f8f24f85a29851364a1168fb9f4f
Darya2302 <119890590+Darya2302@users.noreply.github.com>
2023-10-17 22:33:13 +0300

    Add files via upload
    part 2

--- lab1_linux/main.cpp
+++ lab1_linux/main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
-#include <csignal>
 #include <unistd.h>
-
-// Функция-обработчик сигнала
-void signalHandler(int signal) {
-    std::cout << "Received signal: " << signal << std::endl;
-    exit(signal);
-}
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-    // Установка обработчика сигнала
-    signal(SIGINT, signalHandler);
+    pid_t pid;
+    int status;
 
-    std::cout << "Waiting for signal..." << std::endl;
+    pid = fork();
 
-    while (true) {
-        // Бесконечный цикл ожидания сигнала
-        sleep(1);
+    if (pid == -1) {
+        std::cerr << "Failed to fork process." << std::endl;
+        return 1;
+    } else if (pid == 0) {
+        // Дочерний процесс
+        std::cout << "Child process created." << std::endl;
+        sleep(5); // Для демонстрации, что основной процесс ждет сигнала о завершении
+        exit(0); // Завершаем дочерний процесс
+    } else {
+        // Основной процесс
+        std::cout << "Main process created." << std::endl;
+
+        // Ждем завершения дочернего процесса
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status)) {
+            std::cout << "Child process exited normally with exit code: " << WEXITSTATUS(status) << std::endl;
+        } else if (WIFSIGNALED(status)) {
+            std::cout << "Child process exited due to signal: " << WTERMSIG(status) << std::endl;
+        } else if (WIFSTOPPED(status)) {
+            std::cout << "Child process stopped due to signal: " << WSTOPSIG(status) << std::endl;
+        } else {
+            std::cout << "Child process exited abnormally." << std::endl;
+        }
     }
 
     return 0;
